94795a8e7039aba62679d05139436b45e81e345c
Valeriy Dyakonov <Valeriy.Dyakonov@waveaccess.ru>
2022-04-19 22:49:19 +0400

    Initial commit

---
+++ src/main/java/com/example/GLabRestApiApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GLabRestApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GLabRestApiApplication.class, args);
+    }
+}
